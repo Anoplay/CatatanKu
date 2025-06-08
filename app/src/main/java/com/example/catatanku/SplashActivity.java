ae2649ac360aca028e7d3561090f6c7cc7d2034b
Anoplay <agungprasetyo1111@gmail.com>
2025-06-08 17:45:15 +0700

    first commit

---
+++ app/src/main/java/com/example/catatanku/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.catatanku;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    DBConfig config;
+    SQLiteDatabase db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        config = new DBConfig(this);
+        db = config.getReadableDatabase();
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        }, 1500);
+    }
+}
